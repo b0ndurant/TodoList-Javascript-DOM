28136b04975709198601a6ab54dd232c2e062653
b0ndurant <martin.aurelien01@gmail.com>
2019-04-06 22:38:13 +0200

    add min on input quantity of index.html and add function addTable(), createTable(), createTr() in main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+let i = 1;
+function addTable() {
+	let name = document.getElementById("name").value;
+	let quantity = document.getElementById("quantity").value
+
+	if(name != '') {
+
+		let valueUpper = name[0].toUpperCase() + name.slice(1);
+
+		if (!document.querySelector('table')) {
+			createTable(valueUpper);
+			createTr(valueUpper, quantity);
+			btnDelete();
+		}
+		else {
+			createTr(valueUpper, quantity);
+		}
+	}
+}
+
+function createTable(valueUpper) {
+	let table = document.createElement('table');
+	table.innerHTML = '<thead><tr><th scope="col">#</th><th scope="col">Article</th><th scope="col">Quantité</th><th scope="col">Actions</th></tr></thead>'
+	if (document.body.hasAttribute('class')) {
+		table.className = "table dark-mode";
+	}
+	else
+	{
+		table.className = "table";
+	}
+	document.querySelector('h2').after(table);
+}
+
+function createTr(valueUpper, quantity) {
+	let table = document.querySelector('table');
+	let tr = document.createElement('tr');
+	tr.className = "list";
+	tr.innerHTML = '<th scope="row">'+i+'</th> <td>' + valueUpper +'</td><td>'+ quantity + '</td><td><i class="fas fa-trash-alt" onclick="remove(this)"></i></td>';
+	tr.setAttribute('onclick','this.classList.toggle(\'line\')');
+	table.append(tr);
+	document.getElementById("myForm").reset();
+	i++;
+}
